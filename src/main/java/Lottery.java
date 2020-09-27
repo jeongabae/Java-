6aec67132ba912e673c024c2d698cbfdae1600eb
jeongabae <gabea52@gmail.com>
2020-09-27 14:42:47 +0900

    [create] Lottery

--- src/main/java/Lottery.java
+++ src/main/java/Lottery.java
@@ -1,3 +1,23 @@
-public class Lottery {
+import java.io.*;
 
+public class Lottery {
+    public static void main(String[] args) {
+        try {
+
+            File file = new File("/Users/jeongabae/IdeaProjects/homework/src/main/java/text1.txt");
+
+            FileReader filereader = new FileReader(file);
+
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while ((line = bufReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            bufReader.close();
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
