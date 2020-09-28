f8980249a9c1368de6d96f5eadf53557849b0507
jeongabae <gabea52@gmail.com>
2020-09-28 17:02:49 +0900

    [update]correct

--- src/main/java/Correct.java
+++ src/main/java/Correct.java
@@ -1,13 +1,72 @@
-public class Correct {
+public class Correct extends Prize {
     int correct;
+    int bonus;
+    Boolean win = false;
+    int rank;
+
 
     void correctUp() {
         ++correct;
     }
 
-    void correctRe() {
-        if (correct == 6) {
-            System.out.println("축하합니다 1등입니다!");
+    void bonusUp() {
+        ++bonus;
+    }
+
+    void winLottery() {
+        if (3 <= correct && correct <= 6) {
+            win = true;
         }
     }
+
+    void correctRe() {
+        switch (correct) {
+            case 6:
+                rank = 1;
+                break;
+            case 5:
+                if (bonus == 1) {
+                    rank = 2;
+
+                } else {
+                    rank = 3;
+                }
+                break;
+            case 4:
+                rank = 4;
+                break;
+
+            case 3:
+                rank = 5;
+                break;
+        }
+
+    }
+
+    void rankNum() {
+        switch (rank) {
+            case 1:
+                result.add("1등");
+                break;
+            case 2:
+                result.add("2등");
+
+                break;
+            case 3:
+                result.add("3등");
+
+                break;
+            case 4:
+                result.add("4등");
+
+                break;
+            case 5:
+                result.add("5등");
+                break;
+            default:
+                result.add("꽝");
+        }
+    }
+
 }
+
